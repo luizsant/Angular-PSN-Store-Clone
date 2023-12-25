d49aefdeb5a448b6f153be28550503fd0a062cb1
luizsant <luizsant@gmail.com>
2023-12-25 11:18:54 -0300

    finalizando o projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { CardComponent } from './components/card/card.component';
+import { CardLabelComponent } from './components/card/card-label/card-label.component';
+import { CardPriceComponent } from './components/card/card-price/card-price.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     MenuBarComponent,
-    CardComponent
+    CardComponent,
+    CardLabelComponent,
+    CardPriceComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  @Input()
+  gameCover: string = '';
+  @Input() gameLabel: string = '';
+  @Input() gameType: string = '';
+  @Input() gamePrice: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
